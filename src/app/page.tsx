e1479693389a48d35c017e2ebbcd3803262ec30d
Ersin SİVAZ <e.sivaz@globalhashtech.com>
2023-03-25 23:55:15 +0300

    fetch data from tmdb api in the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Results from "@/components/Results";
 
-const inter = Inter({ subsets: ['latin'] })
+const apiKey = process.env.API_KEY;
+export default async function Home({searchParams} : any) {
+  const genre = searchParams.genre || "fetchTrendng";
 
-export default function Home() {
+  const apiUrl = `https://api.themoviedb.org/3/${
+    genre === "fetchTopRated"?"movie/top_rated":"trending/all/week"
+  }?api_key=${apiKey}&language=en-US&page=1`;
+
+  const res = await fetch(apiUrl,{next:{revalidate:10000}});
+  if(!res.ok){
+    throw new Error("Failed to fetch data");
+  }
+  const data = await res.json();
+  const results = data.results;
+
   return (
-    <div className='text-red-400'>HOME</div>
+    <div>
+      <Results results={results} />
+    </div>
   )
 }
