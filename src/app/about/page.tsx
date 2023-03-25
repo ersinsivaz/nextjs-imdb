4d9600c60f6d49e6930fdbf2f6f27b7c58910c87
Ersin SİVAZ <e.sivaz@globalhashtech.com>
2023-03-25 13:11:25 +0300

    created about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore eum libero modi autem non tenetur officiis. Sequi commodi, illo, cumque inventore quis, voluptate consequatur earum eveniet incidunt ea labore tenetur!
+            Sapiente, ea. Ad quibusdam, dolores nemo numquam explicabo dicta laborum maiores voluptas officiis ullam ratione ipsum doloremque accusamus aliquam hic quod aliquid impedit ipsa? Facere perferendis accusamus at tenetur dolorum.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ut rerum, voluptate alias placeat modi beatae nam harum natus quae sed nisi voluptates eveniet id laudantium perspiciatis dolores maxime? Pariatur.
+            Id eum suscipit animi et eligendi, modi ducimus illo perferendis doloremque enim in deserunt voluptas cum quibusdam earum. Commodi officia asperiores excepturi exercitationem rerum enim est cupiditate in labore quos.
+        </p>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos eaque iure nisi sit mollitia quo modi laborum recusandae laboriosam nihil aspernatur doloribus culpa nostrum, error, reiciendis quae doloremque, corporis cupiditate.
+            Adipisci rerum nam quia quisquam, placeat provident minus aperiam assumenda reprehenderit porro nihil animi eaque sapiente reiciendis dolore nulla perferendis veritatis. Debitis atque animi sapiente soluta quasi dolorem facilis? Nisi!
+            Dicta ipsa molestias omnis error. Non, ad temporibus, odio ea excepturi odit aliquid tenetur, ipsa optio esse molestiae sit suscipit tempora consequuntur? Ipsum delectus officia aut totam quam, molestias recusandae!
+        </p>
+    </div>
+  )
+}
